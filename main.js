a695b8b8228d2f9e960e1f5c733e20109e6c3399
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-05 16:37:10 +0200

    rev-1

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 let kmPercorsi = parseInt(prompt("Quanti Kilometri devi percorrere?"));
 let età = parseInt(prompt("Quanti anni hai?"));
-let newest;
+let sconti;
 let prezzoBiglietto = kmPercorsi * 0.21;
 
 const scontoMinorenni = prezzoBiglietto * 20 / 100;
 const scontoOver = prezzoBiglietto * 40 / 100;
 
 if (età <= 21) {
-    newest = prezzoBiglietto - scontoMinorenni;
+    sconti = prezzoBiglietto - scontoMinorenni;
 } else if (età >= 65) {
-    newest = prezzoBiglietto - scontoOver;
+    sconti = prezzoBiglietto - scontoOver;
 }
 
 document.getElementById("km-percorsi").innerHTML = kmPercorsi;
 document.getElementById("anni").innerHTML = età;
-document.getElementById("prezzo-biglietto").innerHTML = Number(newest).toFixed(2);
+document.getElementById("prezzo-biglietto").innerHTML = Number(sconti).toFixed(2);
