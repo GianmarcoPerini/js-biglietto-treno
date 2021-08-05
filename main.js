884b0ecf6e36df916e12c850bdb1ec250c098c28
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-05 17:58:38 +0200

    rev-4

--- main.js
+++ main.js
@@ -6,17 +6,16 @@ let prezzoBiglietto = kmPercorsi * 0.21;
 const scontoMinorenni = prezzoBiglietto * 20 / 100;
 const scontoOver = prezzoBiglietto * 40 / 100;
 
-if (età <= 21) {
+if (età <= 18) {
     sconti = prezzoBiglietto - scontoMinorenni
-    document.getElementById("prezzo-ridotto").innerHTML = Number(sconti).toFixed(2)
+    document.getElementById("prezzo-biglietto").innerHTML = Number(sconti).toFixed(2)
     document.getElementById("sconto-20").innerHTML = "SCONTO 20%";
 } else if (età >= 65) {
     sconti = prezzoBiglietto - scontoOver
-    document.getElementById("prezzo-ridotto").innerHTML = Number(sconti).toFixed(2)
+    document.getElementById("prezzo-biglietto").innerHTML = Number(sconti).toFixed(2)
     document.getElementById("sconto-40").innerHTML = "SCONTO 40%";
 } else {
-    prezzoBiglietto
-    document.getElementById("prezzo-intero").innerHTML = Number(prezzoBiglietto).toFixed(2);
+    document.getElementById("prezzo-biglietto").innerHTML = Number(prezzoBiglietto).toFixed(2);
 
 }
 
